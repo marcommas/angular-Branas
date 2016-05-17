fe9de4e04a7266ec4c4491a69918eab7a74a187f
Marco Comassetto <marcommas@gmail.com>
2016-05-17 16:21:51 -0300

    Aula 08 - Completa

---
+++ Aula-08/js/value/configValue.js
@@ -0,0 +1,12 @@
+/*angular.module("listaTelefonica").value("config", {
+	//Configuração da URL, caso algum dia mude a url, vem somente neste aqui e muda a url, se n era necessario mudar em todos os locais a url
+	baseUrl : "http://localhost:8888/angular-Branas/Aula-08/backend"
+});*/
+
+//Pode ser value ou  constant
+//Difeça que o constant pode ser injetado em serviços do tipo provider
+
+angular.module("listaTelefonica").constant("config", {
+	//Configuração da URL, caso algum dia mude a url, vem somente neste aqui e muda a url, se n era necessario mudar em todos os locais a url
+	baseUrl : "http://localhost:8888/angular-Branas/Aula-08/backend"
+});
